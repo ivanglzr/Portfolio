5987da1da753e448658c3f7e600b2dd7c3e7376e
ivanglzr <ivanglzr01@gmail.com>
2023-09-16 00:17:07 +0200

    Initialized main section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 let toggleThemeBtn = document.querySelector("#toggle-theme-btn");
 
+let toggleThemeIcon = document.querySelector("#toggle-theme-icon");
+let toggleThemeSpan = document.querySelector("#toggle-theme-txt");
+
 toggleThemeBtn.addEventListener("click", () => {
   document.body.classList.toggle("light");
+
+  if (toggleThemeSpan.innerHTML.includes("Light")) {
+    toggleThemeSpan.innerHTML = "Dark Theme";
+    toggleThemeIcon.classList.add("fa-moon");
+    toggleThemeIcon.classList.remove("fa-sun");
+  } else {
+    toggleThemeSpan.innerHTML = "Light Theme";
+    toggleThemeIcon.classList.add("fa-sun");
+    toggleThemeIcon.classList.remove("fa-moon");
+  }
 });
